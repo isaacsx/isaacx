f16aa1543beadd924d03a6a5ca685c0e5350cfb4
isaac <isaacramosflores454@gmail.com>
2024-12-17 13:08:29 -0600

    fix(ui): Fixed navigation issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import { ScrollReveal } from "@/components/ui/scrollReveal";
 import Projects from "@/components/sections/projects";
 import Skills from "@/components/sections/skills";
 import About from "@/components/sections/about";
@@ -7,12 +6,8 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-gradient-to-b from-gray-900 to-black text-white">
       <About />
-      <ScrollReveal>
-        <Skills />
-      </ScrollReveal>
-      <ScrollReveal>
-        <Projects />
-      </ScrollReveal>
+      <Skills />
+      <Projects />
     </main>
   );
 }
